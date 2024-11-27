b12ccda36ca4988166ade60a94f86f28a35bf259
Md. Ashifur Rahaman Sajib <sksajib111@gmail.com>
2024-11-27 19:45:15 +0600

    creating some component and file like jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
 
 import './App.css'
+import Header from './components/Header/Header'
 
 function App() {
  
 
   return (
     <>
+      <Header></Header>
       <h1 className="text-3xl font-bold underline">
       Hello world!
     </h1>
+    <h1>Hello world</h1>
     </>
   )
 }
