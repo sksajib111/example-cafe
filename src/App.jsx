6b85a5484991893e25a7e738289604f739198b51
Md. Ashifur Rahaman Sajib <sksajib111@gmail.com>
2024-12-06 12:27:39 +0600

    clear app.jsx file to ready my project

--- src/App.jsx
+++ src/App.jsx
@@ -8,10 +8,6 @@ function App() {
   return (
     <>
       <Header></Header>
-      <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-    <h1>Hello world</h1>
     </>
   )
 }
