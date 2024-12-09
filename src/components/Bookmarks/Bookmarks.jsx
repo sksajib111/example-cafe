440952b034bd38001f27c6522fe7debc3ba9040b
Md. Ashifur Rahaman Sajib <sksajib111@gmail.com>
2024-12-09 20:13:55 +0600

    create new bookmark section and successfully add blog in clicking bookmark icone

--- src/components/Bookmarks/Bookmarks.jsx
+++ src/components/Bookmarks/Bookmarks.jsx
@@ -1,11 +1,19 @@
+import PropTypes from "prop-types";
+import Bookmark from "../Bookmark/Bookmark";
 
-
-const Bookmarks = ({bookmarks}) => {
+const Bookmarks = ({ bookmarks }) => {
   return (
     <div className="w-1/3 bg-sky-200">
-      <h2 className="text-3xl">this is bookmarks {bookmarks.length}</h2>
+      <h2 className="text-3xl text-center p-4">This is bookmarks: {bookmarks.length}</h2>
+      {bookmarks.map((bookmark) => (
+        <Bookmark key={bookmark.id} bookmark={bookmark}></Bookmark>
+      ))}
     </div>
   );
+};
+
+Bookmarks.propTypes = {
+  bookmarks: PropTypes.array,
 };
 
 export default Bookmarks;
